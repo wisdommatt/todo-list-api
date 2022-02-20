7a66e84772cd19282475f7b835491918361bd806
Wisdom Matthew <talk2wisdommatt@gmail.com>
2022-02-20 18:37:24 +0100

    feat(users): add test for `CreateUser` service method

--- components/users/repository-mocks_test.go
+++ components/users/repository-mocks_test.go
@@ -1,9 +1,7 @@
-package users_test
+package users
 
 import (
 	"context"
-
-	"github.com/wisdommatt/creativeadvtech-assessment/components/users"
 )
 
 // userRepoMock is the user repository mock for mocking the user
@@ -12,24 +10,24 @@ import (
 // application the mocks will/should be generated automatically from
 // interfaces using a tool like https://github.com/vektra/mockery
 type userRepoMock struct {
-	saveUserFunc       func(ctx context.Context, user users.User) (*users.User, error)
-	getUserByIDFunc    func(ctx context.Context, userID string) (*users.User, error)
-	getUsersFunc       func(ctx context.Context, lastID string, limit int) ([]users.User, error)
-	deleteUserByIDFunc func(ctx context.Context, userID string) (*users.User, error)
+	saveUserFunc       func(ctx context.Context, user User) (*User, error)
+	getUserByIDFunc    func(ctx context.Context, userID string) (*User, error)
+	getUsersFunc       func(ctx context.Context, lastID string, limit int) ([]User, error)
+	deleteUserByIDFunc func(ctx context.Context, userID string) (*User, error)
 }
 
-func (r *userRepoMock) saveUser(ctx context.Context, user users.User) (*users.User, error) {
+func (r *userRepoMock) saveUser(ctx context.Context, user User) (*User, error) {
 	return r.saveUserFunc(ctx, user)
 }
 
-func (r *userRepoMock) getUserByID(ctx context.Context, userID string) (*users.User, error) {
+func (r *userRepoMock) getUserByID(ctx context.Context, userID string) (*User, error) {
 	return r.getUserByIDFunc(ctx, userID)
 }
 
-func (r *userRepoMock) getUsers(ctx context.Context, lastID string, limit int) ([]users.User, error) {
+func (r *userRepoMock) getUsers(ctx context.Context, lastID string, limit int) ([]User, error) {
 	return r.getUsersFunc(ctx, lastID, limit)
 }
 
-func (r *userRepoMock) deleteUserByID(ctx context.Context, userID string) (*users.User, error) {
+func (r *userRepoMock) deleteUserByID(ctx context.Context, userID string) (*User, error) {
 	return r.deleteUserByIDFunc(ctx, userID)
 }
