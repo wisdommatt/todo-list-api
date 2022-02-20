4f9d36c414848852b581e525054136d346a29b1b
Wisdom Matthew <talk2wisdommatt@gmail.com>
2022-02-20 18:16:15 +0100

    feat(users): add required/necessary methods to user service

--- components/users/service.go
+++ components/users/service.go
@@ -1,6 +1,9 @@
 package users
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -8,6 +11,10 @@ import (
 // Service is the interface that describes a user
 // service object.
 type Service interface {
+	CreateUser(ctx context.Context, user User) (*User, error)
+	GetUser(ctx context.Context, userID string) (*User, error)
+	GetUsers(ctx context.Context, lastID string, limit int) ([]User, error)
+	DeleteUser(ctx context.Context, userID string) (*User, error)
 }
 
 // userService is the default implementation for Service
@@ -16,6 +23,10 @@ type userService struct {
 	userRepo repository
 	log      *logrus.Logger
 }
+
+var (
+	errSomethingWentWrong = fmt.Errorf("an error occured, please try again later")
+)
 
 // NewService creates a new user service.
 //
@@ -28,4 +39,44 @@ func NewService(mongoDB *mongo.Database, log *logrus.Logger) *userService {
 		userRepo: newRepository(mongoDB),
 		log:      log,
 	}
+}
+
+func (s *userService) CreateUser(ctx context.Context, user User) (*User, error) {
+	log := s.log.WithContext(ctx).WithField("user", user)
+	newUser, err := s.userRepo.saveUser(ctx, user)
+	if err != nil {
+		log.WithError(err).Error("an error occured while creating new user")
+		return nil, errSomethingWentWrong
+	}
+	return newUser, nil
+}
+
+func (s *userService) GetUser(ctx context.Context, userID string) (*User, error) {
+	log := s.log.WithContext(ctx).WithField("userId", userID)
+	user, err := s.userRepo.getUserByID(ctx, userID)
+	if err != nil {
+		log.WithError(err).Error("an error occured while retrieving user")
+		return nil, errSomethingWentWrong
+	}
+	return user, nil
+}
+
+func (s *userService) GetUsers(ctx context.Context, lastID string, limit int) ([]User, error) {
+	log := s.log.WithContext(ctx).WithField("lastId", lastID).WithField("limit", limit)
+	users, err := s.userRepo.getUsers(ctx, lastID, limit)
+	if err != nil {
+		log.WithError(err).Error("an error occured while retrieving users")
+		return nil, errSomethingWentWrong
+	}
+	return users, nil
+}
+
+func (s *userService) DeleteUser(ctx context.Context, userID string) (*User, error) {
+	log := s.log.WithContext(ctx).WithField("userId", userID)
+	user, err := s.userRepo.deleteUserByID(ctx, userID)
+	if err != nil {
+		log.WithError(err).Error("an error occured while deleting user")
+		return nil, errSomethingWentWrong
+	}
+	return user, nil
 }
